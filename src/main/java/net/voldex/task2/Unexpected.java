4b421b59a82bf495b1e99f7389922d11d4f4f6a4
insou22 <zac.kologlu@gmail.com>
2017-09-30 02:10:23 +1000

    Java Task 2 template

---
+++ src/main/java/net/voldex/task2/Unexpected.java
@@ -0,0 +1,15 @@
+package net.voldex.task2;
+
+public final class Unexpected {
+
+	public static void main(String[] args) throws Exception {
+		Unexpected.behaviour();
+		System.out.println("2 + 2 = 4");
+		System.out.println("2 + 2 = " + (2 + 2));
+	}
+
+	private static void behaviour() throws Exception {
+
+	}
+
+}
