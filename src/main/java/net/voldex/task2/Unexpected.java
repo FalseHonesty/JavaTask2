6107cbc03e603b39735a7fbb6be9a80ec84030b5
Unknown <skipboman0@gmail.com>
2017-10-02 17:22:52 -0400

    Set up the basics for accessing the IntegerCache class
    Used reflection to access the "IntegerCache" class. This class is used when calling Integer.valueOf() for values -128 to 127. In theory, setting the value of 4 to 5 will change the math print line, not sure about the simple string.

--- src/main/java/net/voldex/task2/Unexpected.java
+++ src/main/java/net/voldex/task2/Unexpected.java
@@ -1,5 +1,8 @@
 package net.voldex.task2;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
 public final class Unexpected {
 
 	public static void main(String[] args) throws Exception {
@@ -9,7 +12,18 @@ public final class Unexpected {
 	}
 
 	private static void behaviour() throws Exception {
+		//The first subclass of Integer is the IntegerCacheClass
+		Class integerCacheClass = Integer.class.getDeclaredClasses()[0];
 
+		//Get the Integer[] cache class and make it accessible
+		Field integerCacheField = integerCacheClass.getDeclaredField("cache");
+		integerCacheField.setAccessible(true);
+
+		//Actually get the cache
+		Integer[] integerCache = (Integer[]) integerCacheField.get(integerCacheClass);
+
+		//Does this actually work?
+		System.out.println(Arrays.toString(integerCache));
 	}
 
 }
