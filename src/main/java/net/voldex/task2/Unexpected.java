994f4fd6b4e9b0b01f5a89205792b06297a419d0
Unknown <skipboman0@gmail.com>
2017-10-02 18:47:33 -0400

    Changed Integer's field "digits"
    By changing the digits field in the Integer class, all calls to Integer.toString() with '4' will now return '5'. So doing System.out.println(4) will now print 5. However, the main prints still don't print 5.

--- src/main/java/net/voldex/task2/Unexpected.java
+++ src/main/java/net/voldex/task2/Unexpected.java
@@ -1,6 +1,7 @@
 package net.voldex.task2;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
 public final class Unexpected {
@@ -15,19 +16,47 @@ public final class Unexpected {
 		//The first subclass of Integer is the IntegerCacheClass
 		Class integerCacheClass = Integer.class.getDeclaredClasses()[0];
 
-		//Get the Integer[] cache class and make it accessible
+		//Get the Integer[] cache field and make it accessible
 		Field integerCacheField = integerCacheClass.getDeclaredField("cache");
 		integerCacheField.setAccessible(true);
 
 		//Actually get the cache
 		Integer[] integerCache = (Integer[]) integerCacheField.get(integerCacheClass);
 
-		//Set the index of 4 to be the value 5
+		//Set the index of 4 to be the value 5 - This works, but only if Integer.valueOf() is called.
+		//I'm leaving this in because I want all 4's to be 5's.
 		integerCache[132] = 5;
 
-		//Has this changed the array, and possibly the print methods?
-		System.out.println(Arrays.toString(integerCache));
-		System.out.println(Integer.valueOf(4));
+		//Get the base integer class
+		Class integerClass = Integer.class;
+
+		//Get the char[] digits field and make it accessible - should probably be moved to a new method
+		//but I'm only allowed to change this method.
+		Field charDigitsField = integerClass.getDeclaredField("digits");
+		charDigitsField.setAccessible(true);
+
+		//Get the array of chars from the integer class.
+
+		char[] digits = {
+				'0' , '1' , '2' , '3' , '5' , '5' ,
+				'6' , '7' , '8' , '9' , 'a' , 'b' ,
+				'c' , 'd' , 'e' , 'f' , 'g' , 'h' ,
+				'i' , 'j' , 'k' , 'l' , 'm' , 'n' ,
+				'o' , 'p' , 'q' , 'r' , 's' , 't' ,
+				'u' , 'v' , 'w' , 'x' , 'y' , 'z'
+		};
+
+		charDigitsField.setAccessible(true);
+
+		//Get the int modifiers field and make it accessible, and also remove the final modifier from
+		//char digits field.
+		Field modifiersField = Field.class.getDeclaredField("modifiers");
+		modifiersField.setAccessible(true);
+		modifiersField.setInt(charDigitsField, charDigitsField.getModifiers() & ~Modifier.FINAL);
+
+		charDigitsField.set(null, digits);
+
+		System.out.println(4);
 	}
 
 }
