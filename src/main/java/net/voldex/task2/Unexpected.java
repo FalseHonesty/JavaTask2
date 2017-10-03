7b03d7260aeecf6c481496ce0dc4c3fe190f01e5
Unknown <skipboman0@gmail.com>
2017-10-03 17:13:14 -0400

    Went with a completely new method
    Instead of changing the Integer class, I edited the cached version of the string "2 + 2 = 4" to make it equal 5.

--- src/main/java/net/voldex/task2/Unexpected.java
+++ src/main/java/net/voldex/task2/Unexpected.java
@@ -1,8 +1,6 @@
 package net.voldex.task2;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
 
 public final class Unexpected {
 
@@ -13,50 +11,22 @@ public final class Unexpected {
 	}
 
 	private static void behaviour() throws Exception {
-		//The first subclass of Integer is the IntegerCacheClass
-		Class integerCacheClass = Integer.class.getDeclaredClasses()[0];
+		// Create a new string of the value to be printed so it gets cached
+		String controlString = "2 + 2 = 4";
 
-		//Get the Integer[] cache field and make it accessible
-		Field integerCacheField = integerCacheClass.getDeclaredField("cache");
-		integerCacheField.setAccessible(true);
+		// Get the string class
+		Class stringClass = String.class;
 
-		//Actually get the cache
-		Integer[] integerCache = (Integer[]) integerCacheField.get(integerCacheClass);
+		// Get the char[] value
+		Field stringValueField = stringClass.getDeclaredField("value");
+		stringValueField.setAccessible(true);
 
-		//Set the index of 4 to be the value 5 - This works, but only if Integer.valueOf() is called.
-		//I'm leaving this in because I want all 4's to be 5's.
-		integerCache[132] = 5;
+		// Set the '4' to be a '5' in our control string
+		char[] controlStringValue = controlString.toCharArray();
+		controlStringValue[8] = '5';
 
-		//Get the base integer class
-		Class integerClass = Integer.class;
-
-		//Get the char[] digits field and make it accessible - should probably be moved to a new method
-		//but I'm only allowed to change this method.
-		Field charDigitsField = integerClass.getDeclaredField("digits");
-		charDigitsField.setAccessible(true);
-
-		//Get the array of chars from the integer class.
-
-		char[] digits = {
-				'0' , '1' , '2' , '3' , '5' , '5' ,
-				'6' , '7' , '8' , '9' , 'a' , 'b' ,
-				'c' , 'd' , 'e' , 'f' , 'g' , 'h' ,
-				'i' , 'j' , 'k' , 'l' , 'm' , 'n' ,
-				'o' , 'p' , 'q' , 'r' , 's' , 't' ,
-				'u' , 'v' , 'w' , 'x' , 'y' , 'z'
-		};
-
-		charDigitsField.setAccessible(true);
-
-		//Get the int modifiers field and make it accessible, and also remove the final modifier from
-		//char digits field.
-		Field modifiersField = Field.class.getDeclaredField("modifiers");
-		modifiersField.setAccessible(true);
-		modifiersField.setInt(charDigitsField, charDigitsField.getModifiers() & ~Modifier.FINAL);
-
-		charDigitsField.set(null, digits);
-
-		System.out.println(4);
+		// Make our control string our newly edited char array, thus changing the cached version
+		stringValueField.set(controlString, controlStringValue);
 	}
 
 }
